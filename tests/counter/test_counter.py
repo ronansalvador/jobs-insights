17d0453dc0992ee5c18de34e47a84305357fe845
Ronan Salvador <ronansalvador@yahoo.com.br>
2023-02-16 19:10:47 -0300

    feat(test): teste para a função count_ocurrences, req10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,13 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    path = 'data/jobs.csv'
+    assert count_ocurrences(path, 'Javascript') == 122
+    assert count_ocurrences(path, 'javascript') == 122
+    assert count_ocurrences(path, 'GitHub') == 43
+    assert count_ocurrences(path, 'github') == 43
+    assert count_ocurrences(path, 'React') == 141
+    assert count_ocurrences(path, 'react') == 141
+    assert count_ocurrences(path, 'Python') == 1639
+    assert count_ocurrences(path, 'python') == 1639
