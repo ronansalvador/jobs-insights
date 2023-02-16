59e36381a14c1d29dbfe0335e1c1a69c9c28981f
Ronan Salvador <ronansalvador@yahoo.com.br>
2023-02-16 16:41:54 -0300

    feat(req6): função filter_by_job_type, retorna uma lista com todos os empregos onde a coluna job_type corresponde ao parâmetro job_type

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -59,4 +59,9 @@ def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
     list
         List of jobs with provided job_type
     """
-    raise NotImplementedError
+    # raise NotImplementedError
+    filtered = []
+    for job in jobs:
+        if job["job_type"] == job_type:
+            filtered.append(job)
+    return filtered
