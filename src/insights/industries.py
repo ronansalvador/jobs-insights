33164901bdb159f37de9746ca98e3eb525d28586
Ronan Salvador <ronansalvador@yahoo.com.br>
2023-02-16 16:09:12 -0300

    feat(req 3): função get_unique_industries, retorna  uma lista de valores únicos presentes na coluna industry.

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,4 +1,5 @@
 from typing import List, Dict
+from src.insights.jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
@@ -16,7 +17,12 @@ def get_unique_industries(path: str) -> List[str]:
     list
         List of unique industries
     """
-    raise NotImplementedError
+    # raise NotImplementedError
+    industries = set()
+    for row in read(path):
+        if row['industry']:
+          industries.add(row["industry"])
+    return industries
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
